5f0284366e28e0ae55ef7e0e84ace81a4684ea01
Maxim <Maxim@141.72.159.0>
2015-07-03 11:18:59 +0200

    Klasse "Spielfeld" mit Methode "befülleSpielfeld" geschrieben

--- src/de/dhstudent/fruitfield/Spielfeld.java
+++ src/de/dhstudent/fruitfield/Spielfeld.java
@@ -1,14 +1,16 @@
 package de.dhstudent.fruitfield;
 
-import java.util.ArrayList;
+
 import java.util.Random;
 
 public class Spielfeld {
 	
 		
 	 public void befülleSpieldfeld(){
+		 //Erzeugen eines zweidminensionalen Arrays "Spielfeld"
 	 Beet[][] Spielfeld = new Beet[4][5]; 
 		 	for (int i=0;i<4;i++){
+		 		
 				 for(int j=0; j<5;j++){
 					 
 					 
