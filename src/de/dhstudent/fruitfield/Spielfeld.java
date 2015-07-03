fca6d5f417cba14ccb1c14116da8a8a364f2d3ac
Maxim <Maxim@141.72.159.0>
2015-07-03 11:05:02 +0200

    Klasse Spielfeld mit Methode "befülleSpielfeld" erzeugt

---
+++ src/de/dhstudent/fruitfield/Spielfeld.java
@@ -0,0 +1,29 @@
+package de.dhstudent.fruitfield;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Spielfeld {
+	
+		
+	 public void befülleSpieldfeld(){
+	 Beet[][] Spielfeld = new Beet[4][5]; 
+		 	for (int i=0;i<4;i++){
+				 for(int j=0; j<5;j++){
+					 
+					 
+					 	// Berechnung eines zufälligen Zustandes
+					 	Random rand = new Random();
+					 	int intrandomZustand = rand.nextInt((2 - 0) + 1) + 0;
+					    // Erstellen eines neuen Beetes mit zufälligem Zustand. Zeile und Spalte aus den Schleifenwerten
+					    Beet beet = new Beet(intrandomZustand,i,j);
+					   // Zuweisung des erstellten Beetes zum Spielfeld
+					    Spielfeld[i][j]= beet;
+				 }
+		 
+				 
+			 }
+			
+	 }
+		 
+	 }
